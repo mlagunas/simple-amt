14051e39cf3dd2f95ac2088bdb6161c28e4396bb
mlagunas <mlagunas94@gmail.com>
2016-12-05 11:29:58 +0100

    Adding last changes on accepts/rejects

--- json_reader.py
+++ json_reader.py
@@ -26,33 +26,31 @@ def read_multiple_json(json_file):
 
 train_f = 5
 easy_f = 1
-results_file = "examples/image_sentence/results_50_100.json"
-results_file2 = "examples/image_sentence/results_100_150.json"
+results_file = "examples/image_sentence/results_10_30.json"
+# results_file2 = "examples/image_sentence/results_100_150.json"
+
 train_n = 4
 easy_n = 7
-train_margin = 1 - (train_f/train_n) - 0.01
-easy_margin = 1 - (easy_f/easy_n) - 0.01
-# print train_margin, easy_margin
-# train_margin = 0.49
-# easy_margin = 0.
-data_mturk1 = read_multiple_json(results_file)
-data_mturk2 = read_multiple_json(results_file2)
-data_mturk = []
-for i in range(len(data_mturk1)):
-    data_mturk.append(data_mturk1[i])
-for i in range(len(data_mturk2)):
-    data_mturk.append(data_mturk2[i])
+train_margin = 1 - (train_f / train_n) - 0.01
+easy_margin = 1 - (easy_f / easy_n) - 0.01
+
+data_mturk = read_multiple_json(results_file)
+# data_mturk2 = read_multiple_json(results_file2)
+
+# data_mturk = []
+# for i in range(len(data_mturk1)):
+#     data_mturk.append(data_mturk1[i])
+# for i in range(len(data_mturk2)):
+#     data_mturk.append(data_mturk2[i])
 print len(data_mturk)
 # data_mturk = read_multiple_json(results_file)
 train_failed = 0
 easy_failed = 0
 total_failed = 0
-#
-# a = float(6/7)
-# print a
+
 money = 0
 money_rej = 0
-rejected_asgm = []
+rejected_asgm, approved_asgm = [], []
 for i in range(len(data_mturk)):
     fail = False
     if float(data_mturk[i]['train_acc']) < train_margin:
@@ -62,17 +60,31 @@ for i in range(len(data_mturk)):
         easy_failed += 1
         fail = True
     if fail:
-        print ("worker_id " + data_mturk[i]['worker_id']), "Asgm_id " + data_mturk[i]['assignment_id'], "hit_id " + data_mturk[i]['hit_id']
+        # print ("worker_id " + data_mturk[i]['worker_id']), "Asgm_id " + data_mturk[i]['assignment_id'], "hit_id " + data_mturk[i]['hit_id']
         total_failed += 1
         rejected_asgm.append(data_mturk[i]['assignment_id'])
         money_rej += 0.28
     else:
+        approved_asgm.append(data_mturk[i]['assignment_id'])
         money += 0.28
 
+print "rejected_asgm", len(rejected_asgm), "approved_asgm", len(approved_asgm)
 print "train", train_failed
-print "easy" ,easy_failed
+print "easy", easy_failed
 print "Rejected", total_failed
 print "$ to pay(in theory):", money
 print "rejected $(in theory):", money_rej
 with open('rejected.json', 'w') as outfile:
     json.dump(rejected_asgm, outfile)
+
+print ('This will reject %d assignments and approve %d, with '
+       'sandbox=%s' % (len(rejected_asgm), (len(approved_asgm)), str(args.sandbox)))
+print 'Continue?'
+s = raw_input('(Y/N): ')
+if s == 'Y' or s == 'y':
+    print 'Rejecting assignments'
+    for i in rejected_asgm:
+        mtc.reject_assignment(i, feedback='Accuracy on randomly sampled anti-spam triplets was not high enough.')
+    print 'Approving assignmnets'
+    for i in approved_asgm:
+      mtc.approve_assignment(i)
