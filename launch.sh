b2ff43962fc96108e5b9d462b6fbe26e72726a7c
mlagunas <mlagunas94@gmail.com>
2016-12-26 19:09:35 +0100

    Adding results test TRAIN:0-250

--- launch.sh
+++ launch.sh
@@ -1,6 +1,6 @@
 python launch_hits.py  \
   --html_template=mturk_forma.html \
   --hit_properties_file=hit_properties/image_sentence.json \
-  --input_json_file=examples/mturk_forma/TRAIN:0-250_inputs.json \
-  --hit_ids_file=examples/mturk_forma/TRAIN:0-250_hit_ids.txt \
-  --input_local_json=examples/mturk_forma/TRAIN:0-250_inputs_local.json
+  --input_json_file=examples/mturk_forma/TRAIN:250-500_inputs.json \
+  --hit_ids_file=examples/mturk_forma/TRAIN:250-500_hit_ids.txt \
+  --input_local_json=examples/mturk_forma/TRAIN:250-500_inputs_local.json
